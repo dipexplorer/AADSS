e03f3cc64438ad2b019ebe73f54a7174c91b3036
dip <dipexplorerid23@gmail.com>
2026-03-20 13:33:03 +0530

    refactor: Implement Supabase SSR client setup for both browser and server

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center">
       <h1 className="text-3xl font-bold">AADSS 🚀</h1>
+      <br />
+      <div className="flex h-screen items-center justify-center">
+        <Button className="bg-red-500 hover:bg-red-600 cursor-pointer">
+          Test Button
+        </Button>
+      </div>
     </div>
   );
 }
---
+++ lib/supabase/server.ts
@@ -0,0 +1,22 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export function createClient() {
+  const cookieStore = cookies();
+
+  return createServerClient(supabaseUrl, supabaseAnonKey, {
+    cookies: {
+      async getAll() {
+        return (await cookieStore).getAll();
+      },
+      setAll(cookiesToSet) {
+        cookiesToSet.forEach(async ({ name, value, options }) =>
+          (await cookieStore).set(name, value, options),
+        );
+      },
+    },
+  });
+}
